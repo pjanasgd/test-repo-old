e1e774fae79c15f6b2f1756ef0fb62152eb705ad
pjanas <pjanas@griddynamics.com>
2023-01-17 16:39:11 +0100

    add method and test

---
+++ src/main/java/src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static String hello(){
+        return "Hello world";
+    }
+}
---
+++ src/test/java/src/MainTest.java
@@ -0,0 +1,13 @@
+package src;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void hello_shouldReturnHelloWorld() {
+        String returnedValue = Main.hello();
+        Assertions.assertEquals("Hello World", returnedValue);
+    }
+}
